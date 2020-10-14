8e53bada75cc591eb8c25049d1c6b0b263daebb2
Artemiy Klyachin <artklyachin@gmail.com>
2020-10-15 00:26:48 +0300

    vershion3

--- D.cpp
+++ D.cpp
@@ -9,41 +9,75 @@
 using namespace std;
 
 
+const int INF = 1e9 + 7;
+const int DefaultBufferSize = 4;
+
+class Queue {
+public:
+	Queue() {};
+
+	void Push_to_the_end(int el);
+	void Push_to_the_middle(int el);
+	int Pop();
+
+private:
+	queue<int> part1;
+	deque<int> part2;
+};
+
+
+void Queue::Push_to_the_end(int index)
+{
+	part2.push_back(index);
+	if (part2.size() > part1.size()) {
+		int top = part2.front();
+		part2.pop_front();
+		part1.push(top);
+	}
+}
+
+
+void Queue::Push_to_the_middle(int index)
+{
+	if (part2.size() < part1.size()) {
+		part2.push_front(index);
+	} else {
+		part1.push(index);
+	}
+}
+
+
+int Queue::Pop()
+{
+	int ans = part1.front();
+	part1.pop();
+	if (part1.size() < part2.size()) {
+		int top = part2.front();
+		part1.push(top);
+		part2.pop_front();
+	}
+	return ans;
+}
+
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	queue<int> part1;
-	deque<int> part2;
+	Queue st;
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; ++i) {
 		char c;
-		int j;
+		int visitor_number;
 		cin >> c;
 		if (c == '+') {
-			cin >> j;
-			part2.push_back(j);
-			if (part2.size() > part1.size()) {
-				int top = part2.front();
-				part2.pop_front();
-				part1.push(top);
-			}
+			cin >> visitor_number;
+			st.Push_to_the_end(visitor_number);
 		} else if (c == '*') {
-			cin >> j;
-			if (part2.size() < part1.size()) {
-				part2.push_front(j);
-			} else {
-				part1.push(j);
-			}
+			cin >> visitor_number;
+			st.Push_to_the_middle(visitor_number);
 		} else if (c == '-') {
-			cout << part1.front() << endl;
-			part1.pop();
-			if (part1.size() < part2.size()) {
-				int top = part2.front();
-				part1.push(top);
-				part2.pop_front();
-			}
+			cout << st.Pop() << endl;
 		}
 	}
 	return 0;
