f0027ab86caaa3716566a6e87226307efc99de1e
Artemiy Klyachin <artklyachin@gmail.com>
2020-12-07 12:47:23 +0300

    varsion3

--- E3.cpp
+++ E3.cpp
@@ -17,75 +17,132 @@
 using namespace std;
 
 
+class Sets {
+public:
+    Sets(int64_t m);
+    void Add(int64_t e, int64_t s);
+    void Delete(int64_t e, int64_t s);
+    void Clear(int64_t s);
+    vector<int64_t> Listset(int64_t s) const;
+    vector<int64_t> Listsetsof(int64_t e);
+private:
+    vector<set<int64_t>> S;
+    vector<set<int64_t>> E;
+    vector<int64_t> i_to_num;
+    map<int64_t, int64_t> num_to_i;
+};
+
+Sets::Sets(int64_t m)
+{
+    S.resize(m + 1, set<int64_t>());
+}
+
+
+void Sets::Add(int64_t e, int64_t s)
+{
+    if (num_to_i.find(e) == num_to_i.end()) {
+        i_to_num.push_back(e);
+        num_to_i[e] = int64_t(i_to_num.size()) - 1;
+        E.push_back(set<int64_t>());
+    }
+    int64_t index = num_to_i[e];
+    S[s].insert(index);
+    E[index].insert(s);
+}
+
+
+void Sets::Delete(int64_t e, int64_t s)
+{
+    int64_t index = num_to_i[e];
+    S[s].erase(index);
+    E[index].erase(s);
+}
+
+
+void Sets::Clear(int64_t s)
+{
+    for (int64_t i : S[s]) {
+        E[i].erase(s);
+    }
+    S[s].clear();
+}
+
+
+vector<int64_t> Sets::Listset(int64_t s) const
+{
+    vector<int64_t> ans;
+    if (S[s].empty()) {
+        return ans;
+    }
+    for (auto it = S[s].begin(); it != S[s].end(); ++it) {
+        ans.push_back(i_to_num[*it]);
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+
+vector<int64_t> Sets::Listsetsof(int64_t e)
+{
+    vector<int64_t> ans;
+    if (num_to_i.find(e) == num_to_i.end() || E[num_to_i[e]].empty()) {
+        return ans;
+    }
+    int64_t index = num_to_i[e]; // из-за этого не использую const
+    for (auto it = E[index].rbegin(); it != E[index].rend(); ++it) {
+        ans.push_back(*it);
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int64_t n, m, k;
     cin >> n >> m >> k;
-    vector<set<int64_t>> S(m + 1, set<int64_t>()), E;
-    vector<int64_t> i_to_num;
-    map<int64_t, int64_t> num_to_i;
+    Sets S(m);
     for (int64_t i = 0; i < k; ++i) {
         string st;
         cin >> st;
         if (st == "ADD") {
             int64_t e, s;
             cin >> e >> s;
-            if (num_to_i.find(e) == num_to_i.end()) {
-                i_to_num.push_back(e);
-                num_to_i[e] = int64_t(i_to_num.size()) - 1;
-                E.push_back(set<int64_t>());
-            }
-            int64_t index = num_to_i[e];
-            S[s].insert(index);
-            E[index].insert(s);
+            S.Add(e, s);
         } else if (st == "DELETE") {
             int64_t e, s;
             cin >> e >> s;
-            int64_t index = num_to_i[e];
-            S[s].erase(index);
-            E[index].erase(s);
+            S.Delete(e, s);
         } else if (st == "CLEAR") {
             int64_t s;
             cin >> s;
-            for (int i : S[s]) {
-                E[i].erase(s);
-            }
-            S[s].clear();
+            S.Clear(s);
         } else if (st == "LISTSET") {
             int64_t s;
             cin >> s;
-            if (S[s].empty()) {
+            vector<int64_t> ans = S.Listset(s);
+            if (ans.empty()) {
                 cout << "-1" << endl;
-                continue;
+            } else {
+                for (int64_t i = 0; i < int64_t(ans.size()); ++i) {
+                    cout << ans[i] << ' ';
+                }
+                cout << endl;
             }
-            vector<int64_t> ans;
-            for (auto it = S[s].begin(); it != S[s].end(); ++it) {
-                ans.push_back(i_to_num[*it]);
-            }
-            sort(ans.begin(), ans.end());
-            for (int64_t i = 0; i < int64_t(ans.size()); ++i) {
-                cout << ans[i] << ' ';
-            }
-            cout << endl;
         } else if (st == "LISTSETSOF") {
             int64_t e;
             cin >> e;
-            if (num_to_i.find(e) == num_to_i.end() || E[num_to_i[e]].empty()) {
+            vector<int64_t> ans = S.Listsetsof(e);
+            if (ans.empty()) {
                 cout << "-1" << endl;
-                continue;
+            } else {
+                for (int64_t i = 0; i < int64_t(ans.size()); ++i) {
+                    cout << ans[i] << ' ';
+                }
+                cout << endl;
             }
-            int64_t index = num_to_i[e];
-            vector<int64_t> ans;
-            for (auto it = E[index].rbegin(); it != E[index].rend(); ++it) {
-                ans.push_back(*it);
-            }
-            sort(ans.begin(), ans.end());
-            for (int64_t i = 0; i < int64_t(ans.size()); ++i) {
-                cout << ans[i] << ' ';
-            }
-            cout << endl;
         }
     }
     return 0;
