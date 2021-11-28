d69a2580397ab31e6d9cf4b36249f12db690a81b
artklyachin <artklyachin@gmail.com>
2021-11-29 00:13:58 +0300

    edit task11J

--- 11J.cpp
+++ 11J.cpp
@@ -1,37 +1,23 @@
 ﻿#include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
-#include <random>
-#include <assert.h>
-#include <algorithm>
 
 
-using namespace std;
-
-
-vector<int> z_function(const string& s)
+std::vector<int> z_function(const std::string& text)
 {
-	vector<int> res;
-	int r = 0, l = 0;
-	res.resize(s.size());
-	for (int i = 1; i < s.size(); ++i) {
-		if (r >= i) {
-			res[i] = min(r + 1 - i, res[i - l]);
+	std::vector<int> res;
+	int right = 0, left = 0;
+	res.resize(text.size());
+	for (int i = 1; i < text.size(); ++i) {
+		if (right >= i) {
+			res[i] = std::min(right + 1 - i, res[i - left]);
 		}
-		while (res[i] + i < s.size() && s[res[i]] == s[res[i] + i]) {
+		while (res[i] + i < text.size() && text[res[i]] == text[res[i] + i]) {
 			res[i] += 1;
 		}
-		if (i - 1 + res[i] > r) {
-			l = i;
-			r = i - 1 + res[i];
+		if (i - 1 + res[i] > right) {
+			left = i;
+			right = i - 1 + res[i];
 		}
 	}
 	return res;
@@ -40,36 +26,39 @@ vector<int> z_function(const string& s)
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-	string t, p;
-	int k;
-	cin >> t >> p >> k;
-	int ts = int(t.size()), ps = int(p.size()), shift = ps + 1;
-	string s(ps + 1 + ts, '#');
-	copy(p.begin(), p.end(), s.begin());
-	copy(t.begin(), t.end(), s.begin() + shift);
-	auto pf1 = z_function(s);
-	copy(p.rbegin(), p.rend(), s.begin());
-	copy(t.rbegin(), t.rend(), s.begin() + shift);
-	auto pf2 = z_function(s);
-	vector<int> result;
-	for (int i = 0; i + ps <= ts; ++i) {
-		if (pf1[shift + i] + k + pf2[shift + ts - (ps + i)] >= ps) {
+	std::string text, pattern;
+	int num_of_mistakes;
+	std::cin >> text >> pattern >> num_of_mistakes;
+
+	int text_size = int(text.size()), pattern_size = int(pattern.size()), shift = pattern_size + 1;
+	std::string z_func_str(pattern_size + 1 + text_size, '#');
+
+	copy(pattern.begin(), pattern.end(), z_func_str.begin());
+	copy(text.begin(), text.end(), z_func_str.begin() + shift);
+	auto result1 = z_function(z_func_str);
+
+	copy(pattern.rbegin(), pattern.rend(), z_func_str.begin());
+	copy(text.rbegin(), text.rend(), z_func_str.begin() + shift);
+	auto result2 = z_function(z_func_str);
+
+	std::vector<int> result;
+	for (int i = 0; i + pattern_size <= text_size; ++i) {
+		if (result1[shift + i] + num_of_mistakes + result2[shift + text_size - (pattern_size + i)] >= pattern_size) {
 			result.push_back(i);
 		}
 	}
-	cout << result.size() << endl;
+	std::cout << result.size() << std::endl;
 	for (int& v : result) {
-		cout << v + 1 << " ";
+		std::cout << v + 1 << " ";
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return 0;
 }
 
 
 /*
 mxxgdpylr axg 2                                                                                                         2                                                                                                                       1 2
-
 */
