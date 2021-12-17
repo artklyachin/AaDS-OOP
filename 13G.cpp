3d42df3d118ba2bba33c3234e706ea6ea29610a1
artklyachin <artklyachin@gmail.com>
2021-12-17 22:14:54 +0300

    edit 13G

--- 13G.cpp
+++ 13G.cpp
@@ -2,114 +2,127 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <fstream>
 #include <complex>
 
-using namespace std;
+const double PI = 3.14159265358979323846;
 
-# define PI 3.14159265358979323846
-
-struct Solution
+class Polynomial
 {
-private:
-    static constexpr int64_t MOD = 1e9 + 7;
+    std::vector<int64_t> coefficients_;
 
-    vector<complex<double>> DFT(vector<complex<double>> A, int64_t sign)
+    static int reverse_bits(int number, int bits)
     {
-        if (A.size() == 1) {
-            return A;
+        int result = 0;
+        for (int bit = 0; bit < bits; ++bit)
+            if (number & (1 << bit))
+                result |= 1 << (bits - 1 - bit);
+        return result;
+    }
+
+    static void DFT(std::vector<std::complex<double>>& A, int64_t sign)
+    {
+        int N = static_cast<int>(A.size());
+        int bits = 0;
+        while ((1 << bits) < N) {
+            ++bits;
         }
 
-        int64_t N = A.size();
-        vector<complex<double>> A0, A1;
-        for (int64_t i = 0; i < N / 2; ++i) {
-            A0.push_back(A[2 * i]);
-            A1.push_back(A[2 * i + 1]);
+        for (int i = 0; i < N; ++i) {
+            int j = reverse_bits(i, bits);
+            if (i < j) {
+                std::swap(A[i], A[j]);
+            }
         }
-        vector<complex<double>> values_A0 = DFT(A0, sign);
-        vector<complex<double>> values_A1 = DFT(A1, sign);
 
-        vector<complex<double>> values_A(N);
-        double ang = sign * 2 * PI / N;
-        complex<double> w = 1;
-        for (int64_t i = 0; i < N / 2; ++i) {
-            values_A[i] = values_A0[i] + w * values_A1[i];
-            values_A[i + N / 2] = values_A0[i] - w * values_A1[i];
-            w = complex<double>(cos((i + 1) * ang), sin((i + 1) * ang));
+        for (int block_size = 2; block_size <= N; block_size *= 2) {
+            double ang = 2 * PI / block_size * sign;
+            std::complex<double> step(cos(ang), sin(ang));
+            for (int i = 0; i < N; i += block_size) {
+                std::complex<double> w(1);
+                for (int j = 0; j < block_size / 2; ++j) {
+                    std::complex<double> u = A[i + j];
+                    std::complex<double> v = A[i + j + block_size / 2] * w;
+                    A[i + j] = u + v;
+                    A[i + j + block_size / 2] = u - v;
+                    w *= step;
+                }
+            }
         }
-        return values_A;
     }
 
 public:
-    vector<int64_t> solve(vector<int64_t>& A, vector<int64_t>& B)
+    size_t size() const
     {
-        int64_t ans_len = A.size() + B.size() - 1;
+        return coefficients_.size();
+    }
+
+    std::vector<int64_t>& getCoefficients()
+    {
+        return coefficients_;
+    }
+
+    const std::vector<int64_t>& getCoefficients() const
+    {
+        return coefficients_;
+    }
+
+    Polynomial operator*(const Polynomial& other) const
+    {
+        int64_t result_size = size() + other.size() - 1;
         int64_t N = 1;
-        while (N < ans_len) N *= 2;
-        A.resize(N);
-        B.resize(N);
-        vector<complex<double>> complex_A(N), complex_B(N);
-        for (int64_t i = 0; i < N; ++i) {
-            complex_A[i] = A[i];
+        while (N < result_size) {
+            N *= 2;
         }
-        for (int64_t i = 0; i < N; ++i) {
-            complex_B[i] = B[i];
-        }
+        std::vector<std::complex<double>> complex_A(N), complex_B(N);
+        transform(coefficients_.begin(), coefficients_.end(), complex_A.begin(),
+            [](const int64_t& value) { return static_cast<double>(value); });
+        transform(other.coefficients_.begin(), other.coefficients_.end(), complex_B.begin(),
+            [](const int64_t& value) { return static_cast<double>(value); });
 
-        vector<complex<double>> value_A = DFT(complex_A, 1);
-        vector<complex<double>> value_B = DFT(complex_B, 1);
-        vector<complex<double>> value_C(value_A.size());
-        for (size_t i = 0; i < N; ++i) {
-            value_C[i] = value_A[i] * value_B[i];
-        }
+        DFT(complex_A, 1);
+        DFT(complex_B, 1);
+        transform(complex_A.begin(), complex_A.end(), complex_B.begin(), complex_A.begin(),
+            [](const std::complex<double>& a, const std::complex<double>& b) { return a * b; });
 
-        vector<complex<double>> complex_C = DFT(value_C, -1);
-        for (int64_t i = 0; i < N; ++i) {
-            complex_C[i] /= N;
-        }
-        vector<int64_t> C(N);
-        for (int64_t i = 0; i < N; ++i) {
-            C[i] = int64_t(round(real(complex_C[i])));
-        }
-        C.resize(ans_len);
-        return C;
+        DFT(complex_A, -1);
+        double alpha = 1 / static_cast<double>(N);
+
+        Polynomial result;
+        result.coefficients_.resize(result_size);
+        transform(complex_A.begin(), complex_A.begin() + result_size, result.coefficients_.begin(),
+            [&](const std::complex<double>& value) { return static_cast<int64_t>(round(value.real() * alpha)); });
+        return result;
     }
 };
+
+Polynomial readInput(std::istream& is = std::cin)
+{
+    int n;
+    is >> n;
+    Polynomial pol;
+    pol.getCoefficients().resize(n + 1);
+    for (auto it = pol.getCoefficients().rbegin(); it != pol.getCoefficients().rend(); ++it) {
+        is >> *it;
+    }
+    return pol;
+}
+
+void outputResult(const Polynomial& pol, std::ostream& os = std::cout)
+{
+    os << pol.getCoefficients().size() - 1;
+    for (auto it = pol.getCoefficients().rbegin(); it != pol.getCoefficients().rend(); ++it) {
+        os << " " << *it;
+    }
+    os << std::endl;
+}
 
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    vector<int64_t> A, B;
-    int64_t n, m;
-    cin >> n;
-    for (int64_t i = 0; i <= n; ++i) {
-        int64_t num;
-        cin >> num;
-        A.push_back(num);
-    }
-    cin >> m;
-    for (int64_t i = 0; i <= m; ++i) {
-        int64_t num;
-        cin >> num;
-        B.push_back(num);
-    }
-    reverse(A.begin(), A.end());
-    reverse(B.begin(), B.end());
-    vector<int64_t> C = Solution().solve(A, B);
-    reverse(C.begin(), C.end());
-    cout << int64_t(C.size()) - 1 << " ";
-    for (int64_t i : C) {
-        cout << i << " ";
-    }
-    cout << endl;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    auto A = readInput();
+    auto B = readInput();
+    outputResult(A * B);
     return 0;
 }
-
-/*
-
-4 3
-2 1 0 2
-1 2 3
-*/
